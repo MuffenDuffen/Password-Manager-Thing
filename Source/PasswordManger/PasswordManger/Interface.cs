67facf9fe67482325fb04e44a524a6f09588efde
MuffenDuffen <50796186+MuffenDuffen@users.noreply.github.com>
2021-05-14 18:02:23 +0200

    Started working on user input.

---
+++ Source/PasswordManger/PasswordManger/Interface.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace PasswordManger
+{
+    public static class Interface
+    {
+        public static void LogIn()
+        {
+            Console.WriteLine("PasswordManger Program by Nanojaw studios");
+            string input = AskQuestion("Master Password: ");
+            
+            // Implement Maltes encryptor
+            
+            // return a hashed value that we compare to the masterPassword
+            
+            // Get heavily encrypted master password from a file
+            const string masterPassword = "PASSWORD";
+            
+           
+            if (input == masterPassword)
+            {
+                GetPasswords();
+            }
+        }
+
+        private static void GetPasswords()
+        {
+            Console.WriteLine("\nYou are successfully logged in!");
+            Console.WriteLine("Type Done to exit, type Help for more information");
+            string input = AskQuestion("What would you like to do? ");
+        }
+
+        private static string AskQuestion(string question)
+        {
+            Console.Write(question);
+            string answer = Console.ReadLine();
+
+            while (answer == null)
+            {
+                Console.Write(question);
+                answer = Console.ReadLine();
+            }
+            
+            return answer;
+        }
+    }
+}
--- Source/PasswordManger/PasswordManger/Program.cs
+++ Source/PasswordManger/PasswordManger/Program.cs
@@ -6,7 +6,7 @@ namespace PasswordManger
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("PasswordManger Program");
+            Interface.LogIn();
         }
     }
 }
