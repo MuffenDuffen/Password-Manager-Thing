31c1dc6e3e04862ef85a11e8de4f1fe75cf00628
MuffenDuffen <50796186+MuffenDuffen@users.noreply.github.com>
2021-05-14 16:48:30 +0200

    more base commits

--- Source/PasswordManger/PasswordManger/Program.cs
+++ Source/PasswordManger/PasswordManger/Program.cs
@@ -2,11 +2,11 @@
 
 namespace PasswordManger
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("PasswordManger Program");
         }
     }
 }
