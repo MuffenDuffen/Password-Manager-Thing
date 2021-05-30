ecb6e59d90f0a0b83e8e129d76a05ea02c83e978
XpulseIV <malteaxner363@gmail.com>
2021-05-30 11:19:18 +0200

    lkmj dfvzlökm nv

--- Source/PasswordManger/PasswordManager/Program.cs
+++ Source/PasswordManger/PasswordManager/Program.cs
@@ -6,7 +6,12 @@ namespace PasswordManger
     {
         private static void Main(string[] args)
         {
-            latinizeLOL.ReversePrintNumberInLatin(latinizeLOL.PrintLatinNumber("1999999"));
+            var cred = new Credential("GitHub", "test99480@gmail.com", "test");
+
+            var encryptedCred = Encryptor.EncryptCredential(cred, new int[] {5});
+            var decryptedCred = Decryptor.DecryptCredential(encryptedCred, new int[] {5});
+
+            Console.WriteLine("{0} {1}", encryptedCred, decryptedCred.Password);
         }
     }
 }
