d6cc3cdacff37c3d835e8095d4a1bb008f5bc870
MuffenDuffen <50796186+MuffenDuffen@users.noreply.github.com>
2021-05-30 11:46:31 +0200

    tried to implement clippy

--- Source/PasswordManger/PasswordManager/Program.cs
+++ Source/PasswordManger/PasswordManager/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Kolibri;
 
 namespace PasswordManger
 {
@@ -6,13 +7,8 @@ namespace PasswordManger
     {
         private static void Main(string[] args)
         {
-            var cred = new Credential("Hqweelo", "Wqweorqweld", "qweIqwe");
-            
-            string encryptedCred = Encryptor.EncryptCredential(cred, new int[] {5});
-            var decryptedCred = Decryptor.DecryptCredential(encryptedCred, new int[] {5});
-
-            Console.WriteLine(encryptedCred);
-            Console.WriteLine(decryptedCred.AppName);
+            Console.WriteLine(Clippy.PushStringToClipboard("��v'z$*�����ΐhoƞki���|:").OK);
+            Interface.LogIn();
         }
     }
 }
