52c1f50f726669b3b413d4aeca81665a7840deb8
XpulseIV <malteaxner363@gmail.com>
2021-06-12 11:02:51 +0200

    it works i hope

--- Source/PasswordManger/PasswordManager/Program.cs
+++ Source/PasswordManger/PasswordManager/Program.cs
@@ -9,18 +9,18 @@ namespace PasswordManger
             //Interface.LogIn();
 
             var mPass = "testPassTestPassTttT";
-            
+
             var sTring = "testPass";
-            
+
             var encryptionKey = Profile.GetEncryptionKey(mPass);
             var shift = Profile.GetShift(mPass);
             var primeList = PrimeConversionHelperFactory.GeneratePrimeList();
             var dict = PrimeConversionHelperFactory.CreateDictionaryWithNumbersAsKey(primeList);
-            
-            
+
+
             var encrypted = Encryptor.EncryptString(sTring, encryptionKey, shift, dict);
             var decrypted = Decryptor.DecryptString(encrypted, encryptionKey, shift, dict);
-            
+
             Console.WriteLine("{0}, {1}", encrypted, decrypted);
         }
     }
