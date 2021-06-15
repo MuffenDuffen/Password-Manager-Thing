f77e62c2228b7a787fa8d50d66bf6c7813b452fd
XpulseIV <malteaxner363@gmail.com>
2021-06-15 19:37:49 +0200

    brokjen codee canr fix rn lol

---
+++ Source/PasswordManger/PasswordManager/BitReverserOfDoom.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PasswordManger
+{
+    public class BitReverserOfDoom
+    {
+        public static char reverseBitsForChar(char c)
+        {
+            var charNumberInBinary = Convert.ToString(c, 2);
+            var charNumberInBinaryArray = charNumberInBinary.ToCharArray();
+            
+            Array.Reverse(charNumberInBinaryArray);
+            var charNumberInBinaryReversed = new string(charNumberInBinaryArray);
+
+            return (char) Convert.ToInt32(charNumberInBinaryReversed, 2);
+        }
+    }
+}
