8d4f0014c75e4153f72a9cad505a0c6873c47f2c
XpulseIV <malteaxner363@gmail.com>
2021-06-16 09:04:55 +0200

    twest

--- Source/PasswordManger/PasswordManager/BitReverserOfDoom.cs
+++ Source/PasswordManger/PasswordManager/BitReverserOfDoom.cs
@@ -1,19 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PasswordManger
 {
-    public class BitReverserOfDoom
+    internal static class BitReverserOfDoom
     {
-        public static string reverseBitOrder(string encrypt)
+        private static readonly Dictionary<int, int> Dict = new();
+
+        public static string ReverseLol(string text)
         {
-            var encrypted = "";
-            foreach (char c in encrypt)
+            var finalString = "";
+            for (var i = 0; i < text.Length; i++)
             {
-                var binary = Convert.ToString(c, 2);
-                while (binary.Length <= 16) { binary = binary.Insert(0, 0);}
-                encrypted += Encryptor.reverseString(binary);
+                var charInInt = (long) text[i];
+                var charInIntInBinary = Convert.ToString(charInInt, 2);
+
+                var reversedBinary = Encryptor.reverseString(charInIntInBinary);
+                var length = reversedBinary.Length;
+
+                if (!Dict.ContainsKey(Convert.ToInt32(reversedBinary, 2)))
+                {
+                    Dict.Add(Convert.ToInt32(reversedBinary, 2), length);
+                }
+
+                var reversedBinaryInInt = Convert.ToInt32(reversedBinary, 2);
+
+                var reversedBinaryInIntChar = (char) reversedBinaryInInt;
+                finalString += reversedBinaryInIntChar;
             }
-            return encrypted;
+
+            return finalString;
+        }
+
+        public static string ReveseReverseLol(string text)
+        {
+            var finalString = "";
+            for (var i = 0; i < text.Length; i++)
+            {
+                var charInInt = (int) text[i];
+                var charInIntInBinary = Convert.ToString(charInInt, 2).PadLeft(Dict[charInInt], '0');
+
+                var reversedBinary = Encryptor.reverseString(charInIntInBinary);
+
+                var reversedBinaryInInt = Convert.ToInt32(reversedBinary, 2);
+
+                var reversedBinaryInIntChar = (char) reversedBinaryInInt;
+                finalString += reversedBinaryInIntChar;
+            }
+
+            return finalString;
         }
     }
 }
--- Source/PasswordManger/PasswordManager/Program.cs
+++ Source/PasswordManger/PasswordManager/Program.cs
@@ -10,36 +10,43 @@ namespace PasswordManger
         {
             //Interface.LogIn();
 
-            var mPass = "HAHAIaSAKBAD2";
-            var passPhrase = "LoL" + Profile.GetPassPhrase(mPass);
-            
-            var encryptionKey = Profile.GetEncryptionKey(mPass);
-            foreach (var key in encryptionKey) Console.WriteLine(key);
-            var shift = Profile.GetShift(mPass);
+            // var mPass = "HAHAIaSAKBAD2";
+            // var passPhrase = "LoL" + Profile.GetPassPhrase(mPass);
+            //
+            // var encryptionKey = Profile.GetEncryptionKey(mPass);
+            // foreach (var key in encryptionKey) Console.WriteLine(key);
+            // var shift = Profile.GetShift(mPass);
+            //
+            // var cred = new Credential("testAppName", "testEmail", "testPass");
+            //
+            // var stopWatch = new Stopwatch();
+            //
+            // stopWatch.Start();
+            // var eC = Encryptor.EncryptCredential(cred, encryptionKey, shift, passPhrase);
+            // var dC = Decryptor.DecryptCredential(eC, encryptionKey, shift, passPhrase);
+            // stopWatch.Stop();
+            //
+            // var ts = stopWatch.Elapsed;
+            //
+            // Console.WriteLine(eC);
+            //
+            // Console.WriteLine("---");
+            //
+            // Console.WriteLine(ts);
+            // Console.WriteLine("---");
+            // Console.WriteLine("LOL");
+            // Console.WriteLine("------------------");
+            //
+            // Console.WriteLine(dC.AppName);
+            // Console.WriteLine(dC.Email);
+            // Console.WriteLine(dC.Password);
 
-            var cred = new Credential("testAppName", "testEmail", "testPass");
-            
-            var stopWatch = new Stopwatch();
+            string t = "HAHAIaSAKBAD2lOLImBest";
 
-            stopWatch.Start();
-            var eC = Encryptor.EncryptCredential(cred, encryptionKey, shift, passPhrase);
-            var dC = Decryptor.DecryptCredential(eC, encryptionKey, shift, passPhrase);
-            stopWatch.Stop();
+            var eS = BitReverserOfDoom.ReverseLol(t);
+            var dS = BitReverserOfDoom.ReveseReverseLol(eS);
 
-            var ts = stopWatch.Elapsed;
-
-            Console.WriteLine(eC);
-
-            Console.WriteLine("---");
-
-            Console.WriteLine(ts);
-            Console.WriteLine("---");
-            Console.WriteLine("LOL");
-            Console.WriteLine("------------------");
-            
-            Console.WriteLine(dC.AppName);
-            Console.WriteLine(dC.Email);
-            Console.WriteLine(dC.Password);
+            Console.WriteLine(dS);
         }
     }
 }
